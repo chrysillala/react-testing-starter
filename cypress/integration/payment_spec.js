187b5f8d0e65d8bce2d9a5ec02181a0504838794
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2022-04-21 22:19:23 +0700

    add login flow

--- cypress/integration/payment_spec.js
+++ cypress/integration/payment_spec.js
@@ -1,6 +1,12 @@
 describe("payment", () => {
   it("user can make payment", () => {
     // login
+    cy.visit("/");
+    cy.findByRole("textbox", { name: /username/i }).type("johndoe");
+    cy.findByLabelText(/password/i).type("s3cret");
+    cy.findByRole("checkbox", { name: /remember me/i }).check();
+    cy.findByRole("button", { name: /sign in/i }).click();
+
     // check balance
     // click new
     // search for user

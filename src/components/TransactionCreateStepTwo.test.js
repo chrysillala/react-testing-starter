4aa1c30b27cb66f1233aaebc2eed27c604be214e
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2022-04-21 16:49:11 +0700

    add falsy passed test

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 
+// this one should failed because we want the pay button to be disabled
+// but it passed
+// why this happened?
+// when you go to new transaction > select contact
+// you can see for a fraction of time the button is enabled
+// this happens because we use formik
 test("on initial render, the pay button should be disabled", () => {
   render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
-  // to console log the <body> that RTL can find
-  screen.debug();
+  expect(screen.getByRole("button", { name: /pay/i })).toBeEnabled();
 });

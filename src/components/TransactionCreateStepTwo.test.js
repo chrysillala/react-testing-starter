7c8fa4a45740516a8f00760604ef736ea1f64c82
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2022-04-21 16:57:15 +0700

    make the test passed
    truthy passed

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -7,14 +7,14 @@ import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 // when you go to new transaction > select contact
 // you can see for a fraction of time the button is enabled
 // this happens because we use formik
-test("on initial render, the pay button should be disabled - falsy", () => {
-  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
-  expect(screen.getByRole("button", { name: /pay/i })).toBeEnabled();
-});
+// test("on initial render, the pay button should be disabled - falsy", () => {
+//   render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
+//   expect(screen.getByRole("button", { name: /pay/i })).toBeEnabled();
+// });
 
 // we need async await to fix the falsy passed test above
 // and screen.findByRole
 test("on initial render, the pay button should be disabled", async () => {
   render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
-  expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
 });

850e232c24060dbbd6593212411d7eddea8496dd
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2022-04-21 22:04:17 +0700

    add test plan for user can make payment

---
+++ cypress/integration/payment_spec.js
@@ -0,0 +1,13 @@
+describe("payment", () => {
+  it("user can make payment", () => {
+    // login
+    // check balance
+    // click new
+    // search for user
+    // insert amount and note
+    // click pay
+    // return to transactions
+    // verify if payment made
+    // verify if payment amount was deducted
+  });
+});
